adf82f43dc7a754e6c68606654059f99c74e299b
Ekachcka <2331286yan@gmail.com>
2022-08-23 15:00:08 +0300

    кенкен

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const { DB_URL, DB_NAME } = require("../constans");
 
+
 async function start() {
+  console.log(`${DB_URL,DB_NAME}`)
   await mongoose.connect(`${DB_URL}`, { dbName: DB_NAME });
 }
 
